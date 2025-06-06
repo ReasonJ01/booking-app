04499bb00b1c2cfa462d2fd07511e6434a3f3227
ed20jr <ed20jr@leeds.ac.uk>
2025-06-06 15:07:01 +0100

    Impliemnt better-auth, add protected views, update desktopnav bar

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,13 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "../lib/generated/prisma";
+import { prisma } from "./prisma";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    adapter: prismaAdapter(prisma, {
+        provider: "postgresql"
     }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: true,
+    },
 });
