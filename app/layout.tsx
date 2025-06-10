ea2b9aab6379b659ee3b4c37cc625d4b816e3cb6
ed20jr <ed20jr@leeds.ac.uk>
2025-06-10 20:02:39 +0100

    Apply theme before render to prevent flash of lightmode before darkmode is applied

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,19 @@ import "./globals.css";
 import MobileNavBar from "@/components/MobileNavBar";
 import DesktopNavBar from "@/components/DesktopNavBar";
 
+// Theme script that runs before page render
+const themeScript = `
+  let isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const stored = localStorage.getItem('theme')
+  
+  if (stored === 'dark' || (!stored && isDark)) {
+    document.documentElement.classList.add('dark')
+  }
+  
+  if (stored === 'light') {
+    document.documentElement.classList.remove('dark')
+  }
+`
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +44,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full" suppressHydrationWarning>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} antialiased min-h-full flex flex-col`}
       >
